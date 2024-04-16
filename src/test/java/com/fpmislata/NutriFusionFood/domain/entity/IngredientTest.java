5ce48a7fcf1f69fc4358cb73a32a8b91b4a12a32
AndreuAlfonso <andalfmeg@alu.edu.gva.es>
2024-04-16 09:07:23 +0200

    Added Ingredient Test

---
+++ src/test/java/com/fpmislata/NutriFusionFood/domain/entity/IngredientTest.java
@@ -0,0 +1,73 @@
+package com.fpmislata.NutriFusionFood.domain.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class IngredientTest {
+    Ingredient ingredient;
+    @Test
+    void voidConstructor() {
+        ingredient = new Ingredient(); 
+    }
+
+    @Test
+    void createIngredientWithoutType() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+    }
+
+    @Test
+    void createIngredientWithType() {
+        Type type = new Type(1, "verdura", "vegetable");
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2, type);
+    }
+
+    @Test
+    void checkId() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals(1, ingredient.getId());
+    }
+
+    @Test
+    void checkGluten() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals(false, ingredient.isGluten());
+    }
+
+    @Test
+    void checkLactose() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals(false, ingredient.isLactose());
+    }
+
+    @Test
+    void checkNameEs() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals("tomate", ingredient.getName_es());
+    }
+
+    @Test
+    void checkNameEn() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals("tomato", ingredient.getName_en());
+    }
+
+    @Test
+    void checkStartSeason() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals(1, ingredient.getStartSeason());
+    }
+
+    @Test
+    void checkEndSeason() {
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2);
+        assertEquals(2, ingredient.getEndSeason());
+    }
+
+    @Test
+    void checkType() {
+        Type type = new Type(1, "verdura", "vegetable");
+        ingredient = new Ingredient(1, false, false, "tomate", "tomato", 1, 2, type);
+        assertEquals(type, ingredient.getType());
+    }
+}
