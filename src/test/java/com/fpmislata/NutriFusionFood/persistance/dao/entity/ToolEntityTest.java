6bfc3c63eb4027c8d27d3df96c6467ecd8c537cc
AndreuAlfonso <andalfmeg@alu.edu.gva.es>
2024-04-16 09:51:07 +0200

    Added ToolEntity Test

---
+++ src/test/java/com/fpmislata/NutriFusionFood/persistance/dao/entity/ToolEntityTest.java
@@ -0,0 +1,32 @@
+package com.fpmislata.NutriFusionFood.persistance.dao.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ToolEntityTest {
+    ToolEntity toolEntity;
+
+    @Test
+    void createAllParameters() {
+        toolEntity = new ToolEntity(1, "sarten", "pan");
+    }
+
+    @Test
+    void checkId() {
+        toolEntity = new ToolEntity(1, "sarten", "pan");
+        assertEquals(1, toolEntity.getId());
+    }
+
+    @Test
+    void checkNameEs() {
+        toolEntity = new ToolEntity(1, "sarten", "pan");
+        assertEquals("sarten", toolEntity.getName_es());
+    }
+
+    @Test
+    void checkNameEn() {
+        toolEntity = new ToolEntity(1, "sarten", "pan");
+        assertEquals("pan", toolEntity.getName_en());
+    }
+}
