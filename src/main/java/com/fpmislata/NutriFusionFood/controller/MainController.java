ea77b4b175284e105b85f7d47b88790997f95793
Angela Hermosilla del Rio <angelahermosillad@gmail.com>
2024-04-29 12:27:03 +0200

    Change Sass functions to personalize bootstrap

--- src/main/java/com/fpmislata/NutriFusionFood/controller/MainController.java
+++ src/main/java/com/fpmislata/NutriFusionFood/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
     @GetMapping("/home")
-    public String showMainPage(){
-        return "home";
+    public String showMainPage() {
+        return "homecopy";
     }
 }
