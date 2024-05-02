35ca1391f6473c802ce2b1454dc6aa4352b1333e
AndreuAlfonso <andalfmeg@alu.edu.gva.es>
2024-05-02 11:28:26 +0200

    Modify categoy image and tag, add categories to navbar

--- src/main/java/com/fpmislata/NutriFusionFood/controller/MainController.java
+++ src/main/java/com/fpmislata/NutriFusionFood/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.fpmislata.NutriFusionFood.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fpmislata.NutriFusionFood.common.container.CategoryIoC;
+import com.fpmislata.NutriFusionFood.domain.service.CategoryService;
 
 @Controller
 public class MainController {
+
+    CategoryService categoryService;
+
+    public MainController() {
+        this.categoryService = CategoryIoC.getCategoryService();
+    }
+
     @GetMapping("/home")
-    public String showMainPage() {
+    public String showMainPage(Model model) {
+        model.addAttribute("categoryList", categoryService.findAllCategory());
         return "homecopy";
     }
 }
