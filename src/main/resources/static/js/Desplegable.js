2e369f83c4408dfea6938b891da5d6f60bf00323
Soledad Berenguer Reyes <your email>
2024-05-27 13:13:27 +0200

    Add img lang to header

--- src/main/resources/static/js/Desplegable.js
+++ src/main/resources/static/js/Desplegable.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
-    $('.dropdown-menu').click(function(event) {
-        event.stopPropagation();
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownWrap = document.querySelector('.i8-dropdown__wrap');
+    var dropdownLabel = dropdownWrap.querySelector('.i8-dropdown__label');
+
+    dropdownLabel.addEventListener('click', function() {
+        dropdownWrap.classList.toggle('open');
+    });
+
+    // Cerrar el dropdown cuando se hace clic fuera de él
+    document.addEventListener('click', function(event) {
+        if (!dropdownWrap.contains(event.target)) {
+            dropdownWrap.classList.remove('open');
+        }
     });
 });
